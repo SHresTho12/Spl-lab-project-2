c1bcf70a42404fce4c0a15e80e8b67bfa6ef38ea
SHresTho12 <talimul12bari@gmail.com>
2022-02-25 11:25:47 +0600

    homepage finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,10 @@ export function Navbar() {
       borderBottomColor={useColorModeValue('gray.100', 'gray.700')}
       mb={4}
     >
-      <HStack py={4} justifyContent='flex-end' maxW='container.lg' mx='auto'>
+      <HStack py={4} justifyContent='flex-end' maxW='100%' mx='auto'>
         <Navlink to='/' name='Teach Me 2.0' size='lg' />
         <Spacer />
+        {!currentUSer && <Navlink to='/login' name='Login' />}
         {!currentUSer && <Navlink to='/login' name='Login' />}
         {!currentUSer && <Navlink to='/register' name='Register' />}
         {currentUSer  && <Navlink to='/profile' name='Profile' />}
